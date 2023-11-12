acd8d041755cb3762e04db7bc546cb1429ef0885
Luke-Sheltraw <lukesheltraw@gmail.com>
2023-11-11 22:00:49 -0800

    trying other option for camera

--- scripts/post.js
+++ scripts/post.js
@@ -1,39 +1,39 @@
-function initializeListeners() {
-  document.querySelector('#close-video-button').addEventListener('click', () => {
-    window.history.back();
-  }, { once: true });
-}
+// function initializeListeners() {
+//   document.querySelector('#close-video-button').addEventListener('click', () => {
+//     window.history.back();
+//   }, { once: true });
+// }
 
-function initializeStream() {
-  const video = document.querySelector('#video-display');
-  const captureButton = document.querySelector('#take-photo-button');
+// function initializeStream() {
+//   const video = document.querySelector('#video-display');
+//   const captureButton = document.querySelector('#take-photo-button');
 
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
-    .then((stream) => {
-      video.srcObject = stream;
-      video.play();
-    });
+//   navigator.mediaDevices
+//     .getUserMedia({ video: true, audio: false })
+//     .then((stream) => {
+//       video.srcObject = stream;
+//       video.play();
+//     });
 
-  video.addEventListener('canplay', () => {
-    captureButton.addEventListener('click', () => {
-      takePicture(video);
-    });
-  }, { once: true });
-}
+//   video.addEventListener('canplay', () => {
+//     captureButton.addEventListener('click', () => {
+//       takePicture(video);
+//     });
+//   }, { once: true });
+// }
 
-function takePicture(video) {
-  const canvas = document.querySelector('#video-output-canvas');
-  // const photo = document.querySelector('#photo');
+// function takePicture(video) {
+//   const canvas = document.querySelector('#video-output-canvas');
+//   // const photo = document.querySelector('#photo');
 
-  const context = canvas.getContext("2d");
-  context.drawImage(video, 0, 0, 200, 200);
-  // const data = canvas.toDataURL("image/png");
+//   const context = canvas.getContext("2d");
+//   context.drawImage(video, 0, 0);
+//   // const data = canvas.toDataURL("image/png");
 
-  // photo.setAttribute("src", data);
-}
+//   // photo.setAttribute("src", data);
+// }
 
 export function initPost() {
-  initializeListeners();
-  initializeStream();
+  // initializeListeners();
+  // initializeStream();
 }
