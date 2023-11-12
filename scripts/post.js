45f7b6bab905d90ceacca2f5b04c1e1909dbbe43
Luke-Sheltraw <lukesheltraw@gmail.com>
2023-11-11 22:41:40 -0800

    testing new interface

--- scripts/post.js
+++ scripts/post.js
@@ -1,3 +1,21 @@
-export function initPost() {
+function initializeUpload() {
+  const imageUpload = document.querySelector('#image-upload');
+  const imagePreview = document.querySelector('#image-preview');
 
+  const fileReader = new FileReader();
+
+  fileReader.addEventListener('loadend', (e) => {
+    const encoding = e.target.result;
+
+    imagePreview.src = encoding;
+    imagePreview.classList.remove('hidden');
+  });
+
+  imageUpload.addEventListener('change', () => {
+    fileReader.readAsDataURL(imageUpload.files[0]);
+  });
+}
+
+export function initPost() {
+  initializeUpload();
 }
